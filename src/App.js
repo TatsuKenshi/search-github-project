9da53f55c5bc137fb37f0c0e68f8b5ee686d3cb2
TatsuKenshi <rockinbg@gmail.com>
2022-02-24 22:07:20 +0100

    routing continued

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import Loginpage from "./pages/Loginpage";
+import Errorpage from "./pages/Errorpage";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/login" element={<Loginpage />} />
+        <Route path="/*" element={<Errorpage />} />
       </Routes>
     </BrowserRouter>
   );
